cc80a3c22da88a7c1e73e94de88a072e4533c6d8
matthewgeorgy <matthewgeorgy61@gmail.com>
2025-07-04 17:17:49 -0400

    unity build fixed the inlining

--- make.bat
+++ make.bat
@@ -2,17 +2,17 @@
 
 set CPP_INCLUDE=..\include
 set CPP_FLAGS=/W4 /O2 /Zi /MP /I %CPP_INCLUDE% /FC /nologo /wd4201 /EHsc
-set CPP_SOURCE=..\source\*.cpp
+set CPP_SOURCE=..\source\build.cpp
 
 pushd build\
 
-REM cl %CPP_FLAGS% /D SIMD_WIDTH=1 /arch:AVX /Fe"main_1x" %CPP_SOURCE% user32.lib gdi32.lib
-REM cl %CPP_FLAGS% /D SIMD_WIDTH=4 /arch:AVX /Fe"main_4x" %CPP_SOURCE% user32.lib gdi32.lib
-REM cl %CPP_FLAGS% /D SIMD_WIDTH=8 /arch:AVX /Fe"main_8x" %CPP_SOURCE% user32.lib gdi32.lib
+cl %CPP_FLAGS% /D SIMD_WIDTH=1 /arch:AVX /Fe"main_1x" %CPP_SOURCE% user32.lib gdi32.lib
+cl %CPP_FLAGS% /D SIMD_WIDTH=4 /arch:AVX /Fe"main_4x" %CPP_SOURCE% user32.lib gdi32.lib
+cl %CPP_FLAGS% /D SIMD_WIDTH=8 /arch:AVX /Fe"main_8x" %CPP_SOURCE% user32.lib gdi32.lib
 
-clang++ ..\source\*.cpp -O2 -g -I ..\include -D SIMD_WIDTH=1 -mavx -march=haswell -luser32.lib -lgdi32.lib -o main_1x.exe -Wno-endif-labels -fdiagnostics-absolute-paths
-clang++ ..\source\*.cpp -O2 -g -I ..\include -D SIMD_WIDTH=4 -mavx -march=haswell -luser32.lib -lgdi32.lib -o main_4x.exe -Wno-endif-labels -fdiagnostics-absolute-paths
-clang++ ..\source\*.cpp -O2 -g -I ..\include -D SIMD_WIDTH=8 -mavx -march=haswell -luser32.lib -lgdi32.lib -o main_8x.exe -Wno-endif-labels -fdiagnostics-absolute-paths
+REM clang++ ..\source\*.cpp -O2 -g -I ..\include -D SIMD_WIDTH=1 -mavx -march=haswell -luser32.lib -lgdi32.lib -o main_1x.exe -Wno-endif-labels -fdiagnostics-absolute-paths
+REM clang++ ..\source\*.cpp -O2 -g -I ..\include -D SIMD_WIDTH=4 -mavx -march=haswell -luser32.lib -lgdi32.lib -o main_4x.exe -Wno-endif-labels -fdiagnostics-absolute-paths
+REM clang++ ..\source\*.cpp -O2 -g -I ..\include -D SIMD_WIDTH=8 -mavx -march=haswell -luser32.lib -lgdi32.lib -o main_8x.exe -Wno-endif-labels -fdiagnostics-absolute-paths
 
 popd
 
---
+++ source/build.cpp
@@ -0,0 +1,18 @@
+#define _CRT_SECURE_NO_WARNINGS
+#define MG_IMPL
+#include <mg.h>
+#include <bitmap.h>
+#include <fixed_point.h>
+#include <simd.h>
+#include <renderer.h>
+#include <mesh.h>
+
+#include "../source/bitmap.cpp"
+#include "../source/renderer.cpp"
+#include "../source/fixed_point.cpp"
+#include "../source/mesh.cpp"
+#include "../source/simd_1x.cpp"
+#include "../source/simd_4x.cpp"
+#include "../source/simd_8x.cpp"
+#include "../source/main.cpp"
+
